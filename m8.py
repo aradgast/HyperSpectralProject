3221507073063399a47229e3ec136db202a7a111
arad gast <aradgast1@gmail.com>
2022-10-31 15:02:57 +0200

    WIP m8

--- m8.py
+++ m8.py
@@ -2,40 +2,52 @@ import numpy as np
 
 
 def m8(cube):
-    bands, rpic, cpic = cube.shape
-    m8cube = np.zeros(shape=(bands,rpic,cpic))
+    rpic, cpic, bands = cube.shape
+    m8cube = np.zeros(shape=(rpic, cpic, bands))
     rsize = rpic - 1
     csize = cpic - 1
     col = np.array([i for i in range(1, csize)])
-    col_p1 = np.array([i+1 for i in range(1, csize)])
-    col_m1 = np.array([i-1 for i in range(1, csize)])
+    col_p1 = np.array([i + 1 for i in range(1, csize)])
+    col_m1 = np.array([i - 1 for i in range(1, csize)])
     row = np.array([i for i in range(1, rsize)])
-    row_p1 = np.array([i+1 for i in range(1, rsize)])
-    row_m1 = np.array([i-1 for i in range(1, rsize)])
-    m8cube[:,row, col] = (cube[:,row_m1, col_m1] + cube[:,row_m1, col] +
-                           cube[:,row_m1, col_p1] + cube[:,row, col_m1] + cube[:,row, col_p1] +
-                           cube[:,row_p1, col_m1] + cube[:,row_p1, col] + cube[:,row_p1, col_p1]) / 8
+    row_p1 = np.array([i + 1 for i in range(1, rsize)])
+    row_m1 = np.array([i - 1 for i in range(1, rsize)])
+    # print(cube[0:rsize-1, 0:csize-1].shape)
+    # print(cube[0:rsize-1, 1:csize].shape)
+    # print(cube[0:rsize-1, 2:csize+1].shape)
+    # print(cube[1:rsize, 0:csize-1].shape)
+    # print(cube[1:rsize, 2:csize+1].shape)
+    # print(cube[2:rsize+1, 2:csize+1].shape)
+    # print(cube[2:rsize+1, 0:csize].shape)
+    # print(cube[2:rsize+1, 0:csize-1].shape)
+    ## 8 neighbors
+    m8cube[1:rsize, 1:csize, :] = (cube[0:rsize-1, 0:csize-1] + cube[0:rsize-1, 1:csize] +
+                           cube[0:rsize-1, 2:csize+1] + cube[1:rsize, 0:csize-1] + cube[1:rsize, 2:csize+1] +
+                           cube[2:rsize+1, 0:csize-1] + cube[2:rsize+1, 1:csize] + cube[2:rsize+1, 2:csize+1]) / 8
 
+    m8cube[:, 0, col] = (cube[:, 0, col_m1] + cube[:, 1, col_m1] + cube[:, 1, col] + cube[:, 1, col_p1] + cube[:, rsize,
+                                                                                                          col_p1]) / 5
+    m8cube[:, rsize, col] = (cube[:, rsize, col_m1] + cube[:, rsize - 1, col_m1] + cube[:, rsize - 1, col]
+                             + cube[:, rsize - 1, col_p1] + cube[:, rsize, col_p1]) / 5
+    m8cube[:, row, 0] = (cube[:, row_m1, 0] + cube[:, row_m1, 1] + cube[:, row, 1] + cube[:, row_p1, 1]
+                         + cube[:, row_p1, 0]) / 5
+    m8cube[:, row, csize] = (cube[:, row_m1, csize] + cube[:, row_m1, csize - 1] + cube[:, row, csize - 1]
+                             + cube[:, row_p1, csize - 1] + cube[:, row_p1, csize]) / 5
 
-    m8cube[:,0, col] = (cube[:,0, col_m1] + cube[:,1,col_m1] + cube[:,1, col] + cube[:,1, col_p1]+ cube[:,rsize, col_p1])/5
-    m8cube[:,rsize, col] = (cube[:,rsize, col_m1] + cube[:,rsize-1,col_m1] + cube[:,rsize-1, col]
-                        + cube[:,rsize-1, col_p1] + cube[:,rsize, col_p1])/5
-    m8cube[:,row, 0] = (cube[:,row_m1, 0] + cube[:,row_m1,1] + cube[:,row, 1] + cube[:,row_p1, 1]
-                        + cube[:,row_p1, 0])/5
-    m8cube[:,row, csize] = (cube[:,row_m1, csize] + cube[:,row_m1,csize-1] + cube[:,row, csize-1]
-                        + cube[:,row_p1, csize-1] + cube[:,row_p1, csize])/5
-
-    m8cube[:,0,0] = (cube[:,0,1] + cube[:,1,1] + cube[:,1,0])/3
-    m8cube[:,0,csize] = (cube[:,0,csize-1] + cube[:,1, csize-1] + cube[:,1, csize])/3
-    m8cube[:,rsize,0] = (cube[:,rsize-1,0] + cube[:,rsize-1, 1] + cube[:,rsize, 1])/3
-    m8cube[:,rsize,csize] = (cube[:,rsize-1,csize-1] + cube[:,rsize-1, csize] + cube[:,rsize, csize-1])/3
+    m8cube[:, 0, 0] = (cube[:, 0, 1] + cube[:, 1, 1] + cube[:, 1, 0]) / 3
+    m8cube[:, 0, csize] = (cube[:, 0, csize - 1] + cube[:, 1, csize - 1] + cube[:, 1, csize]) / 3
+    m8cube[:, rsize, 0] = (cube[:, rsize - 1, 0] + cube[:, rsize - 1, 1] + cube[:, rsize, 1]) / 3
+    m8cube[:, rsize, csize] = (cube[:, rsize - 1, csize - 1] + cube[:, rsize - 1, csize] + cube[:, rsize,
+                                                                                           csize - 1]) / 3
 
     return m8cube
 
+
 if __name__ == "__main__":
     a = np.array([[0, 0, 0],
-                [1, 0, 1],
-                [0, 1, 0]])
-    b = np.ones(shape=(2,3,3))
+                  [1, 0, 1],
+                  [0, 1, 0]])
+    b = np.ones(shape=(10, 3, 3))
     b[0] = a
-    print(m8(b))    # print(m8(a))
+    b[2] = -a
+    print(m8(b))  # print(m8(a))
--- main_py.py
+++ main_py.py
@@ -7,20 +7,20 @@ from m8 import m8
 
 img = spy.open_image('D1_F12_H2_Cropped_des_Aligned.hdr')
 r, c, s = img.shape
+img_np = img.open_memmap()
+# pca = spy.algorithms.principal_components(img)
+# eigD = pca.eigenvalues  # values are OK but on reverse order
+# eigV = pca.eigenvectors  # because eigenvalues on reverse order so as the vectors
+m8x = m8(img_np)  # it's not a m8 calc, it's a mean for each band
+# phi_x = pca.cov  # values are ok
+#
+# y = pca.transform(img)  # need to check it
+# pca_y = spy.algorithms.principal_components(y)  # same
+# m8y = m8(y)  # same
+# phi_y = pca_y.cov  # we get the same cov as x
 
-pca = spy.algorithms.principal_components(img)
-eigD = pca.eigenvalues  # values are OK
-eigV = pca.eigenvectors  # values not the same, size is ok
-m8x = m8(img)  # it's not a m8 calc, it's a mean for each band
-phi_x = pca.cov  # values are ok
-
-y = pca.transform(img)  # need to check it
-pca_y = spy.algorithms.principal_components(y)  # same
-m8y = m8(y)  # same
-phi_y = pca_y.cov  # we get the same cov as x
-
-nu_x = find_nu(img, m8x, phi_x)
-nu_y = find_nu(y, m8y, phi_y)
+# nu_x = find_nu(img, m8x, phi_x)
+# nu_y = find_nu(y, m8y, phi_y)
 
 plt.imshow(img[:, :, int(input('band:'))].reshape(r, c), cmap='gray')
 # plt.imshow(pca_img[:,:,int(input('band:'))].reshape(r,c), cmap='gray')
