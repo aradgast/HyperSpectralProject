0a191e369b290505334cfd4762362f81ddcca765
aradgast <aradgast1@gmail.com>
2022-10-31 13:19:47 +0200

    WIP - fixing m8

--- m8.py
+++ m8.py
@@ -2,31 +2,40 @@ import numpy as np
 
 
 def m8(cube):
-    rpic, cpic, bands = cube.shape
-    m8cube = np.zeros(shape=(rpic,cpic,bands))
-    col = np.array([i for i in range(1, cpic)])
-    col_p1 = np.array([i+1 for i in range(1, cpic)])
-    col_m1 = np.array([i-1 for i in range(1, cpic)])
-    row = np.array([i for i in range(1, rpic)])
-    row_p1 = np.array([i+1 for i in range(1, rpic)])
-    row_m1 = np.array([i-1 for i in range(1, rpic)])
-    m8cube[row, col, :] = (cube[row_m1, col_m1, :] + cube[row_m1, col, :] +
-                           cube[row_m1, col_p1, :] + cube[row, col_m1, :] + cube[row, col_p1, :] +
-                           cube[row_p1, col_m1, :] + cube[row_p1, col, :] + cube[row_p1, col_p1, :]) / 8
+    bands, rpic, cpic = cube.shape
+    m8cube = np.zeros(shape=(bands,rpic,cpic))
+    rsize = rpic - 1
+    csize = cpic - 1
+    col = np.array([i for i in range(1, csize)])
+    col_p1 = np.array([i+1 for i in range(1, csize)])
+    col_m1 = np.array([i-1 for i in range(1, csize)])
+    row = np.array([i for i in range(1, rsize)])
+    row_p1 = np.array([i+1 for i in range(1, rsize)])
+    row_m1 = np.array([i-1 for i in range(1, rsize)])
+    m8cube[:,row, col] = (cube[:,row_m1, col_m1] + cube[:,row_m1, col] +
+                           cube[:,row_m1, col_p1] + cube[:,row, col_m1] + cube[:,row, col_p1] +
+                           cube[:,row_p1, col_m1] + cube[:,row_p1, col] + cube[:,row_p1, col_p1]) / 8
 
 
-    m8cube[0, col, :] = (cube[0, col_m1, :] + cube[1,col_m1, :] + cube[1, col, :] + cube[1, col_p1,:]
-                        + cube[rpic, col_p1,:])/5
-    m8cube[rpic, col, :] = (cube[rpic, col_m1, :] + cube[rpic-1,col_m1, :] + cube[rpic-1, col, :]
-                        + cube[rpic-1, col_p1,:] + cube[rpic, col_p1,:])/5
-    m8cube[row, 0, :] = (cube[row_m1, 0, :] + cube[row_m1,1, :] + cube[row, 1, :] + cube[row_p1, 1,:]
-                        + cube[row_p1, 0,:])/5
-    m8cube[row, cpic, :] = (cube[row_m1, cpic, :] + cube[row_m1,cpic-1, :] + cube[row, cpic-1, :]
-                        + cube[row_p1, cpic-1,:] + cube[row_p1, cpic,:])/5
+    m8cube[:,0, col] = (cube[:,0, col_m1] + cube[:,1,col_m1] + cube[:,1, col] + cube[:,1, col_p1]+ cube[:,rsize, col_p1])/5
+    m8cube[:,rsize, col] = (cube[:,rsize, col_m1] + cube[:,rsize-1,col_m1] + cube[:,rsize-1, col]
+                        + cube[:,rsize-1, col_p1] + cube[:,rsize, col_p1])/5
+    m8cube[:,row, 0] = (cube[:,row_m1, 0] + cube[:,row_m1,1] + cube[:,row, 1] + cube[:,row_p1, 1]
+                        + cube[:,row_p1, 0])/5
+    m8cube[:,row, csize] = (cube[:,row_m1, csize] + cube[:,row_m1,csize-1] + cube[:,row, csize-1]
+                        + cube[:,row_p1, csize-1] + cube[:,row_p1, csize])/5
 
-    m8cube[0,0,:] = (cube[0,1,:] + cube[1,1,:] + cube[1,0,:])/3
-    m8cube[0,cpic,:] = (cube[0,cpic-1,:] + cube[1, cpic-1, :] + cube[1, cpic, :])/3
-    m8cube[rpic,0,:] = (cube[rpic-1,0,:] + cube[rpic-1, 1, :] + cube[rpic, 1, :])/3
-    m8cube[rpic,cpic,:] = (cube[rpic-1,cpic-1,:] + cube[rpic-1, cpic, :] + cube[rpic, cpic-1, :])/3
+    m8cube[:,0,0] = (cube[:,0,1] + cube[:,1,1] + cube[:,1,0])/3
+    m8cube[:,0,csize] = (cube[:,0,csize-1] + cube[:,1, csize-1] + cube[:,1, csize])/3
+    m8cube[:,rsize,0] = (cube[:,rsize-1,0] + cube[:,rsize-1, 1] + cube[:,rsize, 1])/3
+    m8cube[:,rsize,csize] = (cube[:,rsize-1,csize-1] + cube[:,rsize-1, csize] + cube[:,rsize, csize-1])/3
 
     return m8cube
+
+if __name__ == "__main__":
+    a = np.array([[0, 0, 0],
+                [1, 0, 1],
+                [0, 1, 0]])
+    b = np.ones(shape=(2,3,3))
+    b[0] = a
+    print(m8(b))    # print(m8(a))
