bf23993e295575eec6c7565b8f5e91e8b7c6978d
arad gast <aradgast1@gmail.com>
2022-11-01 19:25:38 +0200

    WIP

--- ArtificialHyperspectral_class.py
+++ ArtificialHyperspectral_class.py
@@ -5,48 +5,51 @@ import spectral as spy
 import matplotlib.pyplot as plt
 
 
-
-class ArtificialHyperspectralCube():
+class ArtificialHyperspectralCube:
     def __init__(self, header):
 
-        originalData = spy.open_image(header)
-        originalData_np = originalData.open_memmap()  # for working with numpy
+        self.data = np.ndarray(0)
+        original_data = spy.open_image(header)
+        original_data_np = original_data.open_memmap()  # for working with numpy
 
-        self.r, self.c, self.s = originalData.shape
-        originalDataPCA = spy.algorithms.principal_components(originalData)
-        # eigD = originalDataPCA.eigenvalues  # values are OK but on reverse order
-        # eigV = originalDataPCA.eigenvectors  # because eigenvalues on reverse order so as the vectors
-        m8originalData = m8(originalData_np)  # it's not a m8 calc, it's a mean for each band
-        covOriginalData = originalDataPCA.cov  # values are ok
+        self.rowNum, self.colNum, self.bandsNum = original_data.shape
+        original_data_pca = spy.algorithms.principal_components(original_data)
+        # eigD = original_data_pca.eigenvalues  # values are OK but on reverse order
+        # eigV = original_data_pca.eigenvectors  # because eigenvalues on reverse order so as the vectors
+        m8original_data = m8(original_data_np)
+        cov_original_data = original_data_pca.cov
 
-        ##### Y cube ############
+        # Y cube ############
 
-        yCube = originalDataPCA.transform(originalData)  # need to check it
-        yCube_np = yCube.image.open_memmap()
-        m8yCube = m8(yCube_np)  # much larger values(bigger in 2 order)
-        for band in range(self.s):
-            yCube_np[:, :, band] *= 1 / np.sqrt(np.var(yCube_np[:, :, band]))
+        y_cube = original_data_pca.transform(original_data)
+        y_cube_np = y_cube.image.open_memmap().copy()
+        m8y_cube = m8(y_cube_np)
+        for band in range(self.bandsNum):
+            y_cube_np[:, :, band] *= 1 / np.sqrt(np.var(y_cube_np[:, :, band]))
 
-        matrix_y = yCube_np.reshape(self.s, self.r * self.c)
-        covYCube = np.cov(matrix_y)
+        matrix_y = y_cube_np.reshape(self.bandsNum, self.rowNum * self.colNum)
+        cov_y_cube = np.cov(matrix_y)
 
-        nuOriginalData = find_nu(originalData, m8originalData, covOriginalData)
-        nuYCube = find_nu(yCube, m8yCube, covYCube)
+        nu_original_data = find_nu(original_data, m8original_data, cov_original_data)
+        nu_y_cube = find_nu(y_cube, m8y_cube, cov_y_cube)
 
-        ##### Z cube ############
-
-        self.data = np.zeros(shape=(self.r, self.c, self.s))
-
-        for band in range(self.s):
-            self.data[:, :, band] += np.random.standard_t(nuYCube(band), size=(self.r, self.c))
-            self.data[:, :, band] *= 1 / np.sqrt(np.var(z[:, :, band]))
-
-        matrix_z = self.data.reshape(s, r * c)
-        self.cov = np.cov(matrix_z)
+        # Z cube ############
+        self.create_z_cube(nu_y_cube)
+        self.matrix_z = self.data.reshape(self.bandsNum, self.rowNum * self.colNum)
+        self.cov = np.cov(self.matrix_z)
         self.m8 = m8(z)
         self.nu = find_nu(z, self.m8, self.cov)
+
+    def create_z_cube(self, nu_vec):
+        self.data = np.zeros(shape=(self.rowNum, self.colNum, self.bandsNum))
+        for band in range(self.bandsNum):
+            self.data[:, :, band] += np.random.standard_t(nu_vec[band], size=(self.rowNum, self.colNum))
+            self.data[:, :, band] *= 1 / np.sqrt(np.var(self.data[:, :, band]))
 
 
 if __name__ == "__main__":
     z = ArtificialHyperspectralCube('D1_F12_H2_Cropped_des_Aligned.hdr')
     plt.plot([i for i in range(len(z.nu))], z.nu)
+    plt.plot([i for i in range(len(z.m8))], z.m8)
+    plt.show()
+    pass
--- main_py.py
+++ main_py.py
@@ -3,47 +3,57 @@ import matplotlib.pyplot as plt
 import numpy as np
 from find_nu import find_nu
 from m8 import m8
+from ArtificialHyperspectral_class import ArtificialHyperspectralCube
 
 if __name__ == "__main__":
-    # Loading the data
-
-    img = spy.open_image('D1_F12_H2_Cropped_des_Aligned.hdr')
-    img_np = img.open_memmap()  # for working with numpy
-
-    r, c, s = img.shape
-    pca = spy.algorithms.principal_components(img)
-    eigD = pca.eigenvalues  # values are OK but on reverse order
-    eigV = pca.eigenvectors  # because eigenvalues on reverse order so as the vectors
-    m8x = m8(img_np)  # it's not a m8 calc, it's a mean for each band
-    phi_x = pca.cov  # values are ok
-
-    # Y cube ############
-
-    y = pca.transform(img)  # need to check it
-    y_np = y.image.open_memmap().copy()
-    for band in range(s):
-        y_np[:, :, band] *= 1 / np.sqrt(np.var(y_np[:, :, band]))
-
-    m8y = m8(y_np)  # much larger values(bigger in 2 order)
-    matrix_y = y_np.reshape(s, r * c)
-    phi_y = np.cov(matrix_y)
-
-    nu_x = find_nu(img, m8x, phi_x)
-    nu_y = find_nu(y_np, m8y, phi_y)
-
-    # Z cube ############
-
-    z = np.zeros(shape=(r, c, s))
-
-    for band in range(s):
-        z[:, :, band] += np.random.standard_t(nu_y[band], size=(r, c))
-        z[:, :, band] *= 1 / np.sqrt(np.var(z[:, :, band]))
-
-    matrix_z = z.reshape(s, r * c)
-    phi_z = np.cov(matrix_z)
-    m8z = m8(z)
-    nu_z = find_nu(z, m8z, phi_z)
-
-    plt.imshow(img[:, :, int(input('band:'))].reshape(r, c), cmap='gray')
-    # # plt.imshow(pca_img[:,:,int(input('band:'))].reshape(r,c), cmap='gray')
+    z = ArtificialHyperspectralCube('D1_F12_H2_Cropped_des_Aligned.hdr')
+    plt.figure(1)
+    plt.plot([i for i in range(len(z.nu))], z.nu, title='nu')
+    plt.figure(2)
+    plt.plot([i for i in range(len(z.m8))], z.m8, title='m8')
+    plt.figure(3)
+    plt.imshow(z.data[:, :, 0].reshape(z.rowNum, z.colNum), cmap='gist_rainbow', title="'0' band")
     plt.show()
+
+    # # Loading the data
+    #
+    # img = spy.open_image('D1_F12_H2_Cropped_des_Aligned.hdr')
+    # img_np = img.open_memmap()  # for working with numpy
+    #
+    # r, c, s = img.shape
+    # pca = spy.algorithms.principal_components(img)
+    # eigD = pca.eigenvalues  # values are OK but on reverse order
+    # eigV = pca.eigenvectors  # because eigenvalues on reverse order so as the vectors
+    # m8x = m8(img_np)  # it's not a m8 calc, it's a mean for each band
+    # phi_x = pca.cov  # values are ok
+    #
+    # # Y cube ############
+    #
+    # y = pca.transform(img)  # need to check it
+    # y_np = y.image.open_memmap().copy()
+    # for band in range(s):
+    #     y_np[:, :, band] *= 1 / np.sqrt((np.var(y_np[:, :, band])))
+    #
+    # m8y = m8(y_np)  # much larger values(bigger in 2 order)
+    # matrix_y = y_np.reshape(s, r * c)
+    # phi_y = np.cov(matrix_y)
+    #
+    # nu_x = find_nu(img, m8x, phi_x)
+    # nu_y = find_nu(y_np, m8y, phi_y)
+    #
+    # # Z cube ############
+    #
+    # z = np.zeros(shape=(r, c, s))
+    #
+    # for band in range(s):
+    #     z[:, :, band] += np.random.standard_t(nu_y[band], size=(r, c))
+    #     z[:, :, band] *= 1 / np.sqrt(np.var(z[:, :, band]))
+    #
+    # matrix_z = z.reshape(s, r * c)
+    # phi_z = np.cov(matrix_z)
+    # m8z = m8(z)
+    # nu_z = find_nu(z, m8z, phi_z)
+    #
+    # plt.imshow(img[:, :, int(input('band:'))].reshape(r, c), cmap='gray')
+    # # # plt.imshow(pca_img[:,:,int(input('band:'))].reshape(r,c), cmap='gray')
+    # plt.show()
