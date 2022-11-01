ae80079010dfa435e90d77645a94e3622b9de3b1
aradgast <aradgast1@gmail.com>
2022-11-01 12:08:39 +0200

    WIP

--- main_py.py
+++ main_py.py
@@ -5,7 +5,7 @@ from find_nu import find_nu
 from m8 import m8
 
 if __name__ == "__main__":
-    ##### Loading the data ############
+    # Loading the data
 
     img = spy.open_image('D1_F12_H2_Cropped_des_Aligned.hdr')
     img_np = img.open_memmap()  # for working with numpy
@@ -17,26 +17,26 @@ if __name__ == "__main__":
     m8x = m8(img_np)  # it's not a m8 calc, it's a mean for each band
     phi_x = pca.cov  # values are ok
 
-    ##### Y cube ############
+    # Y cube ############
 
     y = pca.transform(img)  # need to check it
-    y_np = y.open_memmap()
-    m8y = m8(y_np)  # much larger values(bigger in 2 order)
+    y_np = y.image.open_memmap().copy()
     for band in range(s):
-        y[:, :, band] *= 1 / np.sqrt(np.var(y[:, :, band]))
+        y_np[:, :, band] *= 1 / np.sqrt(np.var(y_np[:, :, band]))
 
-    matrix_y = y.reshape(s, r * c)
+    m8y = m8(y_np)  # much larger values(bigger in 2 order)
+    matrix_y = y_np.reshape(s, r * c)
     phi_y = np.cov(matrix_y)
 
     nu_x = find_nu(img, m8x, phi_x)
-    nu_y = find_nu(y, m8y, phi_y)
+    nu_y = find_nu(y_np, m8y, phi_y)
 
-    ##### Z cube ############
+    # Z cube ############
 
     z = np.zeros(shape=(r, c, s))
 
     for band in range(s):
-        z[:, :, band] += np.random.standard_t(nu_y(band), size=(r, c))
+        z[:, :, band] += np.random.standard_t(nu_y[band], size=(r, c))
         z[:, :, band] *= 1 / np.sqrt(np.var(z[:, :, band]))
 
     matrix_z = z.reshape(s, r * c)
