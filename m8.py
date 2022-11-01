WIP - fixing process and ading z calculation

m8.py
@@ -39,7 +39,10 @@ if __name__ == "__main__":
     a = np.array([[0, 0, 0],
                   [1, 0, 1],
                   [0, 1, 0]])
-    b = np.ones(shape=(10, 3, 3))
-    b[0] = a
-    b[2] = -a
-    print(m8(b))  # print(m8(a))
+    b = np.ones(shape=(3, 3,3))
+    b[:,:,0] = a
+    b[:,:,2] = -a
+    print(b)  # print(m8(a))
+    print(b.shape)
+    print(b.reshape(3,9))
+    print(b)

main_py.py
@@ -4,24 +4,46 @@ import numpy as np
 from find_nu import find_nu
 from m8 import m8
 
+if __name__ == "__main__":
+    ##### Loading the data ############
 
-img = spy.open_image('D1_F12_H2_Cropped_des_Aligned.hdr')
-r, c, s = img.shape
-img_np = img.open_memmap()
-# pca = spy.algorithms.principal_components(img)
-# eigD = pca.eigenvalues  # values are OK but on reverse order
-# eigV = pca.eigenvectors  # because eigenvalues on reverse order so as the vectors
-m8x = m8(img_np)  # it's not a m8 calc, it's a mean for each band
-# phi_x = pca.cov  # values are ok
-#
-# y = pca.transform(img)  # need to check it
-# pca_y = spy.algorithms.principal_components(y)  # same
-# m8y = m8(y)  # same
-# phi_y = pca_y.cov  # we get the same cov as x
+    img = spy.open_image('D1_F12_H2_Cropped_des_Aligned.hdr')
+    img_np = img.open_memmap()  # for working with numpy
 
-# nu_x = find_nu(img, m8x, phi_x)
-# nu_y = find_nu(y, m8y, phi_y)
+    r, c, s = img.shape
+    pca = spy.algorithms.principal_components(img)
+    eigD = pca.eigenvalues  # values are OK but on reverse order
+    eigV = pca.eigenvectors  # because eigenvalues on reverse order so as the vectors
+    m8x = m8(img_np)  # it's not a m8 calc, it's a mean for each band
+    phi_x = pca.cov  # values are ok
 
-plt.imshow(img[:, :, int(input('band:'))].reshape(r, c), cmap='gray')
-# plt.imshow(pca_img[:,:,int(input('band:'))].reshape(r,c), cmap='gray')
-plt.show()
+    ##### Y cube ############
+
+    y = pca.transform(img)  # need to check it
+    y_np = y.open_memmap()
+    m8y = m8(y_np)  # much larger values(bigger in 2 order)
+    for band in range(s):
+        y[:, :, band] *= 1 / np.sqrt(np.var(y[:, :, band]))
+
+    matrix_y = y.reshape(s, r * c)
+    phi_y = np.cov(matrix_y)
+
+    nu_x = find_nu(img, m8x, phi_x)
+    nu_y = find_nu(y, m8y, phi_y)
+
+    ##### Z cube ############
+
+    z = np.zeros(shape=(r, c, s))
+
+    for band in range(s):
+        z[:, :, band] += np.random.standard_t(nu_y(band), size=(r, c))
+        z[:, :, band] *= 1 / np.sqrt(np.var(z[:, :, band]))
+
+    matrix_z = z.reshape(s, r * c)
+    phi_z = np.cov(matrix_z)
+    m8z = m8(z)
+    nu_z = find_nu(z, m8z, phi_z)
+
+    plt.imshow(img[:, :, int(input('band:'))].reshape(r, c), cmap='gray')
+    # # plt.imshow(pca_img[:,:,int(input('band:'))].reshape(r,c), cmap='gray')
+    plt.show()