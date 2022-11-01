a3f79ae5e7efd3c0b7eb0a54bcda3d38e34b9871
aradgast <aradgast1@gmail.com>
2022-11-01 08:18:47 +0200

    WIP - fixing m8

--- m8.py
+++ m8.py
@@ -2,43 +2,35 @@ import numpy as np
 
 
 def m8(cube):
+    """calculate the 8,5,3 neighbors average for subtracting the background"""
+
     rpic, cpic, bands = cube.shape
     m8cube = np.zeros(shape=(rpic, cpic, bands))
     rsize = rpic - 1
     csize = cpic - 1
-    col = np.array([i for i in range(1, csize)])
-    col_p1 = np.array([i + 1 for i in range(1, csize)])
-    col_m1 = np.array([i - 1 for i in range(1, csize)])
-    row = np.array([i for i in range(1, rsize)])
-    row_p1 = np.array([i + 1 for i in range(1, rsize)])
-    row_m1 = np.array([i - 1 for i in range(1, rsize)])
-    # print(cube[0:rsize-1, 0:csize-1].shape)
-    # print(cube[0:rsize-1, 1:csize].shape)
-    # print(cube[0:rsize-1, 2:csize+1].shape)
-    # print(cube[1:rsize, 0:csize-1].shape)
-    # print(cube[1:rsize, 2:csize+1].shape)
-    # print(cube[2:rsize+1, 2:csize+1].shape)
-    # print(cube[2:rsize+1, 0:csize].shape)
-    # print(cube[2:rsize+1, 0:csize-1].shape)
-    ## 8 neighbors
-    m8cube[1:rsize, 1:csize, :] = (cube[0:rsize-1, 0:csize-1] + cube[0:rsize-1, 1:csize] +
-                           cube[0:rsize-1, 2:csize+1] + cube[1:rsize, 0:csize-1] + cube[1:rsize, 2:csize+1] +
-                           cube[2:rsize+1, 0:csize-1] + cube[2:rsize+1, 1:csize] + cube[2:rsize+1, 2:csize+1]) / 8
-    ## TODO
-    m8cube[:, 0, col] = (cube[:, 0, col_m1] + cube[:, 1, col_m1] + cube[:, 1, col] + cube[:, 1, col_p1] + cube[:, rsize,
-                                                                                                          col_p1]) / 5
-    m8cube[:, rsize, col] = (cube[:, rsize, col_m1] + cube[:, rsize - 1, col_m1] + cube[:, rsize - 1, col]
-                             + cube[:, rsize - 1, col_p1] + cube[:, rsize, col_p1]) / 5
-    m8cube[:, row, 0] = (cube[:, row_m1, 0] + cube[:, row_m1, 1] + cube[:, row, 1] + cube[:, row_p1, 1]
-                         + cube[:, row_p1, 0]) / 5
-    m8cube[:, row, csize] = (cube[:, row_m1, csize] + cube[:, row_m1, csize - 1] + cube[:, row, csize - 1]
-                             + cube[:, row_p1, csize - 1] + cube[:, row_p1, csize]) / 5
 
-    m8cube[:, 0, 0] = (cube[:, 0, 1] + cube[:, 1, 1] + cube[:, 1, 0]) / 3
-    m8cube[:, 0, csize] = (cube[:, 0, csize - 1] + cube[:, 1, csize - 1] + cube[:, 1, csize]) / 3
-    m8cube[:, rsize, 0] = (cube[:, rsize - 1, 0] + cube[:, rsize - 1, 1] + cube[:, rsize, 1]) / 3
-    m8cube[:, rsize, csize] = (cube[:, rsize - 1, csize - 1] + cube[:, rsize - 1, csize] + cube[:, rsize,
-                                                                                           csize - 1]) / 3
+    ## 8 neighbors
+    m8cube[1:rsize, 1:csize, :] = (cube[0:rsize - 1, 0:csize - 1] + cube[0:rsize - 1, 1:csize] +
+                                   cube[0:rsize - 1, 2:csize + 1] + cube[1:rsize, 0:csize - 1] + cube[1:rsize,
+                                                                                                 2:csize + 1] +
+                                   cube[2:rsize + 1, 0:csize - 1] + cube[2:rsize + 1, 1:csize] + cube[2:rsize + 1,
+                                                                                                 2:csize + 1]) / 8
+
+    ## 5 neighbors
+    m8cube[0, 1:csize] = (cube[0, 0:csize - 1] + cube[1, 0:csize - 1] + cube[1, 1:csize] + cube[1, 2:csize + 1] +
+                          cube[rsize, 2:csize + 1]) / 5
+    m8cube[rsize, 1:csize] = (cube[rsize, 0:csize - 1] + cube[rsize - 1, 0:csize - 1] + cube[rsize - 1, 1:csize]
+                              + cube[rsize - 1, 2:csize + 1] + cube[rsize, 2:csize + 1]) / 5
+    m8cube[1:rsize, 0] = (cube[0:rsize - 1, 0] + cube[0:rsize - 1, 1] + cube[1:rsize, 1] + cube[2:rsize + 1, 1]
+                          + cube[2:rsize + 1, 0]) / 5
+    m8cube[1:rsize, csize] = (cube[0:rsize - 1, csize] + cube[0:rsize - 1, csize - 1] + cube[1:rsize, csize - 1]
+                              + cube[2:rsize + 1, csize - 1] + cube[2:rsize + 1, csize]) / 5
+
+    ## 3 neighbors
+    m8cube[0, 0] = (cube[0, 1] + cube[1, 1] + cube[1, 0]) / 3
+    m8cube[0, csize] = (cube[0, csize - 1] + cube[1, csize - 1] + cube[1, csize]) / 3
+    m8cube[rsize, 0] = (cube[rsize - 1, 0] + cube[rsize - 1, 1] + cube[rsize, 1]) / 3
+    m8cube[rsize, csize] = (cube[rsize - 1, csize - 1] + cube[rsize - 1, csize] + cube[rsize, csize - 1]) / 3
 
     return m8cube
 
