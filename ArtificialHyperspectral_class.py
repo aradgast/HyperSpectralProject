fixing covariance calculation

ArtificialHyperspectral_class.py
@@ -27,7 +27,7 @@ class ArtificialHyperspectralCube:
         for band in range(self.bandsNum):
             y_cube_np[:, :, band] *= 1 / np.sqrt(np.var(y_cube_np[:, :, band]))
 
-        matrix_y = y_cube_np.reshape(self.bandsNum, self.rowNum * self.colNum)
+        matrix_y = np.transpose(y_cube_np.reshape(self.rowNum * self.colNum, self.bandsNum))
         self.cov_y_cube = np.cov(matrix_y)
 
         nu_original_data = find_nu(self.original_data, self.m8original_data, self.cov_original_data)
@@ -35,7 +35,7 @@ class ArtificialHyperspectralCube:
 
         # Z cube ############
         self.create_z_cube(nu_y_cube)
-        self.matrix_z = self.data.reshape(self.bandsNum, self.rowNum * self.colNum)
+        self.matrix_z = np.transpose(self.data.reshape(self.rowNum * self.colNum, self.bandsNum))
         self.cov = np.cov(self.matrix_z)
         self.m8 = m8(z)
         self.nu = find_nu(z, self.m8, self.cov)
@@ -53,4 +53,7 @@ if __name__ == "__main__":
     plt.plot([i for i in range(len(z.nu))], z.nu)
     plt.plot([i for i in range(len(z.m8))], z.m8)
     plt.show()
+
+
+
     pass