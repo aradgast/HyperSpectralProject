need to fix ROC plotting and add Ath calculation and peak distance

ArtificialHyperspectral_class.py
@@ -1,15 +1,20 @@
+########################################################################################################################
+# All right reserved by BGU, 2023
+# Author: Arad Gast, Ido Levokovich
+# Date: 03/2023
+# Description: this file contains the ArtificialHyperspectralCube class
+
+########################################################################################################################
+
 import numpy as np
-from scipy.ndimage import generic_filter
-from find_nu import find_nu
 from local_mean_covariance import m8, cov8
 from spectral import *
 import matplotlib.pyplot as plt
-from pca import pca
-
+from PCA import pca
 
 
 class ArtificialHyperspectralCube:
-    """ this class initalize an artifcial hyperspectral cube according to the original data that was given by the
+    """ this class initialize an artificial hyperspectral cube according to the original data that was given by the
      header file.
 
      the class contains the following attributes:
@@ -34,13 +39,13 @@ class ArtificialHyperspectralCube:
 
     def __init__(self, header):
         self.data = open_image(header)
-        self.cube = self.data.load()
-        self.rows, self.cols, self.bands = self.data.shape
+        self.cube = self.data.load(dtype='double').copy()
+        self.rows, self.cols, self.bands = self.cube.shape
 
         self.x_mean = m8(self.cube)
         self.x_cov = cov8(self.cube, self.x_mean)
 
-        self.y = pca(self.cube, self.x_mean, self.x_cov)
+        self.y, self.x_eigvec = pca(self.cube, self.x_mean, self.x_cov)
 
         self.y_mean = m8(self.y)
         self.y_cov = cov8(self.y, self.y_mean)
@@ -54,13 +59,26 @@ class ArtificialHyperspectralCube:
         self.cov = cov8(self.artificial_data, self.m8)
         # self.nu = find_nu(self.data, self.m8, self.cov, False)
 
+        # T cube ############
+        self.t, _ = pca(self.artificial_data, self.m8, self.cov)
+        self.t_mean = m8(self.t)
+        self.t_cov = cov8(self.t, self.t_mean)
+
+        # Q cube ############
+        self.q = np.zeros(shape=(self.rows, self.cols, self.bands), dtype='double')
+        for r in range(self.rows):
+            for c in range(self.cols):
+                self.q[r, c, :] = np.dot(self.x_eigvec, self.t[r, c, :])
+        self.q_mean = m8(self.q)
+        self.q_cov = cov8(self.q, self.q_mean)
+
     def __create_z_cube(self, nu_vec):
         self.artificial_data = np.zeros(shape=(self.rows, self.cols, self.bands))
         for band in range(self.bands):
             self.artificial_data[:, :, band] += np.random.standard_t(nu_vec[band], size=(self.rows, self.cols))
             self.artificial_data[:, :, band] /= self.artificial_data[:, :, band].std()
 
-        self.artificial_data = np.array(self.artificial_data)
+        self.artificial_data = np.array(self.artificial_data) + self.y_mean
 
 
 if __name__ == "__main__":

plot_detection_algo.py
@@ -1,3 +1,11 @@
+#############################################################################################################
+# All right reserved by BGU, 2023
+# Author: Arad Gast, Ido Levokovich
+# Date: 03/2023
+# Description: this file contains the plot_detection_algo function
+
+#############################################################################################################
+
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -15,34 +23,35 @@ def plot(axis, hist_wt, hist_nt, inv_cumulative_wt, inv_cumulative_nt, legends, 
     returns: None"""
 
     fig, ax = plt.subplots(2, 2, figsize=(10, 10))
-    title1 = f'{algo_name} - histogram results for'
-    title2 = f'{algo_name} - log10 histogram results for'
-    title3 = f'{algo_name} - inverse cumulative probability for'
-    title4 = f'{algo_name} - ROC curve with limited pfa for'
+    title1 = f'{algo_name} - histogram results for '
+    title2 = f'{algo_name} - log10 histogram results for '
+    title3 = f'{algo_name} - inverse cumulative probability for '
+    title4 = f'{algo_name} - ROC curve with limited pfa for '
+    colors = ['r', 'g', 'b', 'y', 'm', 'c', 'k']
 
     for i in range(len(axis)):
-        ax[0, 0].plot(hist_wt[i][1][1:], hist_wt[i][0], hist_nt[i][1][1:], hist_nt[i][0], '--',
-                      label=[f'{legends[i]}+WT', f'{legends[i]}_NT'])
+        ax[0, 0].plot(hist_wt[i][1][1:], hist_wt[i][0], label = f'{legends[i]}_WT', color=colors[i], linewidth=i%3+1)
+        ax[0,0].plot(hist_nt[i][1][1:], hist_nt[i][0], '--', label=f'{legends[i]}_NT', color=colors[i], linewidth=i%3+1)
         title1 += legends[i]
         ax[0, 0].grid()
         ax[0, 0].legend()
 
-        ax[0, 1].plot(np.log10(hist_wt[i][1][1:]), np.log10(hist_wt[i][0]), np.log10(hist_nt[i][1][1:]),
-                      np.log10(hist_nt[i][0]), '--', label=[f'{legends[i]}+WT', f'{legends[i]}_NT'])
+        ax[0, 1].plot(hist_wt[i][1][1:], np.log10(hist_wt[i][0]),label=f'{legends[i]}_WT', color=colors[i], linewidth=i%3+1)
+        ax[0, 1].plot(hist_nt[i][1][1:], np.log10(hist_nt[i][0]), '--', label=f'{legends[i]}_NT', color=colors[i], linewidth=i%3+1)
         title2 += legends[i]
         ax[0, 1].grid()
         ax[0, 1].legend()
 
-        ax[1, 0].plot(axis[i], inv_cumulative_wt[i], axis[i], inv_cumulative_nt[i], '--',
-                      label=[f'{legends[i]}+WT', f'{legends[i]}_NT'])
+        ax[1, 0].plot(axis[i], inv_cumulative_wt[i], label=f'{legends[i]}_WT', color=colors[i], linewidth=i%3+1)
+        ax[1, 0].plot(axis[i], inv_cumulative_nt[i], '--', label=f'{legends[i]}_NT', color=colors[i], linewidth=i%3+1)
         title3 += legends[i]
         ax[1, 0].grid()
         ax[1, 0].legend()
 
-        ax[1, 1].plot(inv_cumulative_nt[i], inv_cumulative_wt[i], label=[f'{legends[i]}+WT', f'{legends[i]}_NT'])
+        ax[1, 1].plot(inv_cumulative_nt[i], inv_cumulative_wt[i], label=legends[i], color=colors[i], linewidth=i%3+1)
         title4 += legends[i]
-        ax[1, 0].grid()
-        ax[1, 0].legend()
+        ax[1, 1].grid()
+        ax[1, 1].legend()
 
     ax[0, 0].set_title(title1)
     ax[0, 1].set_title(title2)